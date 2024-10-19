04b6ba9d4aca333aae1560971923d6ab211908db
srzou <srzou@outlook.com>
2024-10-19 21:12:02 +0800

    新增 query、trigger、updateStatus 相关demo

---
+++ src/main/java/com/example/snailjob/handler/TestUpdateJobStatusHandler.java
@@ -0,0 +1,35 @@
+package com.example.snailjob.handler;
+
+import com.aizuda.snailjob.client.job.core.openapi.SnailJobOpenApi;
+import com.aizuda.snailjob.common.core.enums.StatusEnum;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestUpdateJobStatusHandler {
+
+    /**
+     * 更新定时任务状态
+     *
+     * @param jobId 定时任务ID
+     * @return
+     */
+    public Boolean updateJobStatus(Long jobId){
+        return SnailJobOpenApi
+                .updateJobStatus(jobId)
+                .setStatus(StatusEnum.NO)
+                .execute();
+    }
+
+    /**
+     * 更新工作流任务状态
+     *
+     * @param workFlowId 工作流ID
+     * @return
+     */
+    public Boolean updateWorkFlowStatus(Long workFlowId){
+        return SnailJobOpenApi
+                .updateWorkFlowStatus(workFlowId)
+                .setStatus(StatusEnum.YES)
+                .execute();
+    }
+}
