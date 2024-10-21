72599676e109bfeeea830988e847e824a7cb63ca
byteblogs168 <598092184@qq.com>
2024-10-21 23:08:18 +0800

    补充更新功能

--- src/main/java/com/example/snailjob/handler/TestUpdateJobStatusHandler.java
+++ src/main/java/com/example/snailjob/handler/TestUpdateJobStatusHandler.java
@@ -13,10 +13,10 @@ public class TestUpdateJobStatusHandler {
      * @param jobId 定时任务ID
      * @return
      */
-    public Boolean updateJobStatus(Long jobId){
+    public Boolean updateJobStatus(Long jobId, Long status) {
         return SnailJobOpenApi
                 .updateJobStatus(jobId)
-                .setStatus(StatusEnum.YES)
+                .setStatus(StatusEnum.YES.getStatus().equals(status.intValue()) ? StatusEnum.YES : StatusEnum.NO)
                 .execute();
     }
 
@@ -24,12 +24,13 @@ public class TestUpdateJobStatusHandler {
      * 更新工作流任务状态
      *
      * @param workFlowId 工作流ID
+     * @param status
      * @return
      */
-    public Boolean updateWorkFlowStatus(Long workFlowId){
+    public Boolean updateWorkFlowStatus(Long workFlowId, Long status) {
         return SnailJobOpenApi
                 .updateWorkFlowStatus(workFlowId)
-                .setStatus(StatusEnum.YES)
+                .setStatus(StatusEnum.YES.getStatus().equals(status.intValue()) ? StatusEnum.YES : StatusEnum.NO)
                 .execute();
     }
 }
